1415f4cfee02440e91e9bf5bb7d9ef0b0e96d5d0
Isaac Hernández <hernandezisaac2142@gmail.com>
2022-11-24 10:07:28 -0700

    Se redujo  el tamaño de las solicitudes

--- src/routes/ReportsRoutes.js
+++ src/routes/ReportsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const ReportsRouter = express.Router();
-const { getMyReports, saveReport, getAllReports, countAllReports, getReportsByDate } = require('../services/ReportService');
+const { getMyReports, saveReport, getAllReports, countAllReports, getReportsByDate, getReportById } = require('../services/ReportService');
 
 ReportsRouter.get('/my-reports/count-all-reports', countAllReports);
 ReportsRouter.get('/my-reports/:department', getMyReports);
 ReportsRouter.get('/my-reports', getAllReports);
 ReportsRouter.post('/my-reports/save-report', saveReport);
 ReportsRouter.post('/my-reports/get-by-date', getReportsByDate);
+ReportsRouter.get('/my-reports/get-by-id/:_id', getReportById);
 
 module.exports = { ReportsRouter };
--- src/services/ReportService.js
+++ src/services/ReportService.js
@@ -1,6 +1,38 @@
 const ReportInterface = require('../interfaces/ReportInterface');
 const DepartmentInterface = require('../interfaces/DepartmentInterface');
 
+const getReportById = async (req, res) => {
+    ReportInterface.find({_id: req.params._id}, async (err, reports) => {
+        if (err) {
+            console.log("error -> " + error);
+            return res.status(500).json("internal error -> " + error);
+        }
+        else {
+            let response = [];
+
+            for (let i = 0; i < Object.keys(reports).length; i++) {
+                await new Promise(next => {
+                    DepartmentInterface.find({ _id: reports[i].department }, (err, departments) => {
+                        if (!err) {
+                            let auxDepartment = {
+                                _id: departments[0]._id,
+                                name: departments[0].name,
+                                color: departments[0].color,
+                                icon: departments[0].icon
+                            }
+                            reports[i].department = JSON.stringify(auxDepartment);
+                            response.push(reports[i]);
+                        }
+                        next();
+                    });
+                });
+            }
+
+            return res.status(200).json(reports);
+        }
+    });
+}
+
 const getAllReports = async (req, res) => {
 
     ReportInterface.find({}, async (err, reports) => {
@@ -125,4 +157,4 @@ const saveReport = async (req, res) => {
     
 }
 
-module.exports = { getMyReports, saveReport, getAllReports, countAllReports, getReportsByDate };
+module.exports = { getMyReports, saveReport, getAllReports, countAllReports, getReportsByDate, getReportById };
